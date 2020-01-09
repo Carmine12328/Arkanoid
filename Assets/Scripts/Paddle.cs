790e2577d3019a04bffe2b12438949f302d7f803
Carmine12328 <carmine12328@gmail.com>
2020-01-10 00:16:15 +0100

    Finished ball moving

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,12 +2,34 @@
 
 public class Paddle : MonoBehaviour
 {
+
+    #region Singleton
+
+    private static Paddle _instance;
+    public static Paddle Instance => _instance;
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    #endregion
+
+
     private Camera mainCamera;
     private float paddleInitialy;
     private float defaultPaddleWidthInPixels = 200;
     private float defaultLeftClamp = 140;
     private float defaultRighttClamp = 400;
     private SpriteRenderer sr;
+    private object initialBallSpeed;
 
     // Start is called before the first frame update
     private void Start()
@@ -31,5 +53,29 @@ public class Paddle : MonoBehaviour
         float mousePositionPixels = Mathf.Clamp(Input.mousePosition.x, leftClamp, rightClamp);
         var mousePosition = mainCamera.ScreenToWorldPoint(new Vector3(mousePositionPixels, 0, 0)).x;
         this.transform.position = new Vector3(mousePosition, paddleInitialy, 0);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.tag == "Ball")
+        {
+            Rigidbody2D ballRb = coll.gameObject.GetComponent<Rigidbody2D>();
+            Vector3 hitPoint = coll.contacts[0].point;
+            Vector3 paddleCenter = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y);
+
+            ballRb.velocity = Vector2.zero;
+
+            float difference = paddleCenter.x - hitPoint.x;
+
+            if(difference > 0)
+            {
+                ballRb.AddForce(new Vector2(-(Mathf.Abs(difference * 200)), BallsManager.initialBallSpeed));
+            }
+            else
+            {
+                ballRb.AddForce(new Vector2((Mathf.Abs(difference * 200)), BallsManager.initialBallSpeed));
+            }
+        }
     }
 }
